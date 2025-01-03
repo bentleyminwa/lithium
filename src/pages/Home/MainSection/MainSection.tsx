352bb92db991a18cbfef1b2dc444c837dc2fff05
bentleyminwa <bentleyminwa@gmail.com>
2025-01-04 00:41:21 +0300

    feat: add filter functionality to MainSection for toggling between images and videos

--- src/pages/Home/MainSection/MainSection.tsx
+++ src/pages/Home/MainSection/MainSection.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { IoCheckmarkSharp } from "react-icons/io5";
 import ImageList from "../../../common/components/ImageList/ImageList";
 import VideoList from "../../../common/components/VideoList/VideoList";
 import { Image, Video } from "../../../common/types/types";
@@ -8,13 +10,42 @@ type MainSectionProps = {
 };
 
 const MainSection = ({ images, videos }: MainSectionProps) => {
+  const [filter, setFilter] = useState("photos");
+
+  const handleFilterChange = (filter: string) => {
+    setFilter(filter);
+  };
+
   return (
     <div className="mt-20 px-10 md:px-20">
-      <h1 className="text-2xl text-[#ff5b0a] font-semibold mb-10">
-        Free Stock Photos
-      </h1>
-      <ImageList images={images} />
-      <VideoList videos={videos} />
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="text-2xl capitalize font-semibold">
+          free stock {filter}
+        </h1>
+        <div className="">
+          <select
+            id="filter"
+            value={filter}
+            onChange={(e) => handleFilterChange(e.target.value)}
+            className="text-[#ff5b0a] text-lg font-bold px-2 py-1 border border-gray-600 bg-gray-600 rounded-full cursor-pointer outline-none hover:border-[#ff5b0a]"
+          >
+            <option value="photos" className="text-green-500 bg-none">
+              {filter === "photos" && (
+                <IoCheckmarkSharp className="inline-block" />
+              )}{" "}
+              Photos
+            </option>
+            <option value="videos">
+              {filter === "videos" && (
+                <IoCheckmarkSharp className="inline-block" />
+              )}{" "}
+              Videos
+            </option>
+          </select>
+        </div>
+      </div>
+      {filter === "photos" && <ImageList images={images} />}
+      {filter === "videos" && <VideoList videos={videos} />}
     </div>
   );
 };
