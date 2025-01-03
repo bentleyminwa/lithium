c70b71bef3f65a0180e4df3354cd8cc8834a0c0d
bentleyminwa <bentleyminwa@gmail.com>
2025-01-03 18:06:08 +0300

    add context for image fetching and display; update Navbar styles and integrate MainSection component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.tsx";
+import { Provider } from "./context/Store.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
-)
+    <Provider>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </StrictMode>
+);
