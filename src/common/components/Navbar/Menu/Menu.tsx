b86ecfbb0f0c7e363cfb8de1174b9bce919887b7
bentleyminwa <bentleyminwa@gmail.com>
2025-01-04 08:18:54 +0300

    refactor: update Hamburger component size in Menu and remove unused imports

--- src/common/components/Navbar/Menu/Menu.tsx
+++ src/common/components/Navbar/Menu/Menu.tsx
@@ -1,15 +1,9 @@
 import Hamburger from "hamburger-react";
-// import { motion } from "framer-motion";
-
-// const logoVariants = {
-//   hidden: { opacity: 0, x: 100 },
-//   visible: { opacity: 1, x: 0, transition: { delay:0.5, type: "spring", stiffness: 120 } },
-// };
 
 const Menu = () => {
   return (
     <div className="text-white">
-      <Hamburger />
+      <Hamburger size={28} />
     </div>
   );
 };
