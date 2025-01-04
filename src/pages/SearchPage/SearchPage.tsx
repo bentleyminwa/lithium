41adc9523f4a89c4ea3a82c8a4f593d8622ef434
bentleyminwa <bentleyminwa@gmail.com>
2025-01-04 15:42:40 +0300

    feat: add page transition animations using framer-motion in App, Home, and SearchPage components

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,16 +1,42 @@
+import { motion } from "framer-motion";
 import Search from "../../common/components/SearchComponent/Search";
 import MainSection from "../Home/MainSection/MainSection";
 
+const PageVariants = {
+  hidden: {
+    x: "100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  exit: {
+    x: "100vw",
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const SearchPage = () => {
   return (
-    <>
+    <motion.div
+      variants={PageVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <div className="pt-32 bg-search-pattern bg-cover bg-center bg-no-repeat h-[30vh] text-gray-100">
         <div className="w-1/2 mx-auto">
           <Search />
         </div>
       </div>
       <MainSection />
-    </>
+    </motion.div>
   );
 };
 
