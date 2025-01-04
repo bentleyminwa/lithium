ed2bb759f29dbff601e475ea743f45427caa49cc
bentleyminwa <bentleyminwa@gmail.com>
2025-01-04 11:23:45 +0300

    feat: add loading state with Loader component in ImageList and VideoList

---
+++ src/common/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="bg-gray-400 rounded-md relative overflow-hidden h-48 w-full">
+      <div className="absolute inset-0 bg-gray-200 animate-shimmer"></div>
+    </div>
+  );
+};
+
+export default Loader;
